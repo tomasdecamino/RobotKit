a470a815d8c14a8cd1f606bac15764ec21e7e5a7
Alex Roberto Vargas Benamburg <alex@vbalex.com>
2016-08-22 22:17:43 -0600

    Agregada cabecera de autoria

--- Constants.h
+++ Constants.h
@@ -1,3 +1,12 @@
+/* @author: Alex Roberto Vargas Benamburg
+ * April 2016
+ * www.funcostarica.org
+
+Made for teaching poruposes www.funcostarica.org
+Released under MIT License
+Copyright (c) 2016  Alex Roberto Vargas Benamburg
+ */
+
 #define REMOTE_1 16724175 //FF30CF        1
 #define REMOTE_2 16718055 //FF18E7        2
 #define REMOTE_3 16743045 //FF7A85        3
